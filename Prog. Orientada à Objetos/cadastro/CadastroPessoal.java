e809457d10cb13dbd28d9f421e6ee6e27c17da20
Giovana Gezuato <giovanagezuato@gmail.com>
2025-02-14 17:40:09 -0300

    Lendo e  mostrando dados

---
+++ Prog. Orientada à Objetos/cadastro/CadastroPessoal.java
@@ -0,0 +1,13 @@
+package cadastro;
+
+public class CadastroPessoal {
+    public static void main(String [] args) {
+        System.out.println("Nome: João");
+        System.out.println("Endereço: Rua Floriano Peixoto, 259");
+        System.out.println("Bairro: Centro");
+        System.out.println("CEP: 15403-094");
+        System.out.println("Cidade: Sorocaba");
+        System.out.println("UF: SP");
+    }
+}
+
---
+++ Prog. Orientada à Objetos/cadastro/InfoUser.java
@@ -0,0 +1,34 @@
+package cadastro;
+
+import java.util.Scanner;
+
+public class InfoUser {
+    public static void main(String [] args) {
+        Scanner teclado = new Scanner(System.in);
+        
+        System.out.println("Nome: ");
+        String nome = teclado.nextLine();
+
+        System.out.println("Endereço: ");
+        String endereco = teclado.nextLine();
+
+        System.out.println("Bairro: ");
+        String bairro = teclado.nextLine();
+
+        System.out.println("CEP: ");
+        String cep = teclado.nextLine();
+
+        System.out.println("Cidade: ");
+        String cidade = teclado.nextLine();
+
+        System.out.println("UF: ");
+        String uf = teclado.nextLine();
+
+        System.out.println("INFORMAÇÕES DO USUÁRIO");
+        System.out.println("Nome: " + nome);
+        System.out.println("Endereço: " + endereco);
+        System.out.println("Bairro: " + bairro);
+        System.out.println("CEP: " + cep);
+        System.out.println("Cidade: " + cidade + " - " + uf);
+    }
+}
